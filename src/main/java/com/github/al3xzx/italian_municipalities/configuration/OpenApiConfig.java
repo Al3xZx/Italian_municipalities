62e003a7d20657dfa411103f880db736bdf962c4
Alessandro Molinaro <alessandro.molinaro96@gmail.com>
2022-09-13 19:42:07 +0200

    OpeApi docs

--- src/main/java/com/github/al3xzx/italian_municipalities/configuration/OpenApiConfig.java
+++ src/main/java/com/github/al3xzx/italian_municipalities/configuration/OpenApiConfig.java
@@ -1,8 +1,10 @@
 package com.github.al3xzx.italian_municipalities.configuration;
 
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +25,13 @@ public class OpenApiConfig {
         .addServersItem(new Server().url(serverUrl).description(serverDescription))
         .info(new Info().title("Italian Municipalities API")
             .description("Italian municipalities")
-            .version("v0.0.1"));
+            .version("v0.0.1")
+            .license(new License()
+                .name("Apache 3.0")
+                .url("https://github.com/Al3xZx/Italian_municipalities/blob/master/LICENSE")
+            ))
+        .externalDocs(new ExternalDocumentation()
+            .description("Italian municipalities GitHub repository")
+            .url("https://github.com/Al3xZx/Italian_municipalities"));
   }
 }
